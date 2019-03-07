51d91a1ab295d7ebad48fe761eb9a198ea8613e9
p_yang <patrizio0512@gmail.com>
2019-03-06 23:19:46 -0500

    done with throughput output to csv, next step will be to do the same for latency and throughput

--- python/sol.py
+++ python/sol.py
@@ -1,51 +1,96 @@
 import os
+import csv
+SRC_NUM = 8
+DEST_NUM = 9
+PACKET_SIZE = 1000
 
-def compute(file):
+throughput_dict = {}
+
+def parse_file(file):
     with open(file) as f:
         lines = f.readlines()
 
-    startTime = -1
-    endTime = -1
-    dataSize = 0
-    packetNum = 0
-    dropNum = 0
+    drop_num = 0
 
-    latencyCount = 0
-    latencySum = 0
-    packetDict = {}
+    #Global start/end time
+    start_time = -1
+    end_time = -1
+
+    # Parse each tcp packet and save in the start_time and end_time in the dict
+    # e.g packet 1 starts at 1.0 and ends at 10.0, then at the end we'll have an entry {1, [1.0, 10.0]}
+    packet_dict = {}
 
     for line in lines:
         arr = line.split(" ")
-        if(line[0]=="r"):
-            if(startTime==-1): startTime = float(arr[1])
-            else: endTime = float(arr[1])
-            dataSize += int(arr[5])
-            if(packetDict.get(arr[10], -1)!=-1):
-                latencySum += (float(arr[1])-packetDict.get(arr[10]))
-                latencyCount+=1
-        if(line[0]=="+"):
-            packetDict[arr[10]] = float(arr[1])
-            packetNum+=1
+
+        # Since we only care about TCP packet here
+        # if arr[4] == "tcp":
+        #     print "Source " + arr[SRC_NUM]
+        #     print "Dest " + arr[DEST_NUM]
+        if(arr[SRC_NUM] != "0.0" or arr[DEST_NUM] != "3.0"):
+            continue
+        time = float(arr[1])
+        if start_time == -1:
+            start_time = time
+        else:
+            end_time = time
+
+        # If we've seen this packet before, then update end time
+        if arr[10] in packet_dict:
+            packet_dict[arr[10]][1] = time
+        # If we never seen this packet before, set time as start time
+        else:
+            packet_dict[arr[10]] = [time, -1]
+
         if (line[0] == "d"):
-            dropNum+=1
-        # print(line)
+            print "packet drop"
+            drop_num+=1
 
-    throughput = dataSize*8/(1000000*(endTime-startTime))
+    name_arr = file.split("_")
+    tcp_v = name_arr[2]
+    band_width = int(name_arr[1][0])
 
-    print("Throughput: ")
-    print(str(throughput)+" Mbps")
+    if tcp_v not in throughput_dict:
+        throughput_dict[tcp_v] = []
 
-    print("Latency: ")
-    print(str(latencySum/latencyCount)+" s")
+    throughput_dict[tcp_v].append(compute_throughput(packet_dict, start_time, end_time))
+    compute_avg_latency(packet_dict)
+    compute_drop_rate(packet_dict, drop_num)
 
-    print("Drop rate: ")
-    print(str(dropNum*1.0/packetNum)+"%")
+def compute_throughput(packet_dict, start_time, end_time):
+    packet_num = len(packet_dict)
+    data_size = packet_num*PACKET_SIZE
+    throughput = data_size*8/(1000000*(end_time-start_time))
+    print("Throughput: {} Mps".format(throughput))
+    return str(throughput)
 
-for file in os.listdir("./results"):
+
+def compute_avg_latency(packet_dict):
+    sum = 0
+    for value in packet_dict.values():
+        sum += value[1]-value[0]
+    print("Latency: {}s".format(sum/ len(packet_dict)))
+    return sum/ len(packet_dict)
+
+def compute_drop_rate(packet_dict, drop_num):
+    print("Drop rate: {}%".format(drop_num*1.0/len(packet_dict)))
+    return drop_num*1.0/len(packet_dict)
+
+
+for file in sorted(os.listdir("./results")):
     arr = file.split("_")
+    if arr[-1] == "12000.tr":
+        continue
     try:
         print "Computing {} TCP with {} bandwidth and packet size {}".format(arr[2], arr[1], arr[-1])
     except:
         print "Invalid filename: " + file
 
-    compute("./results/" + file)
+    parse_file("./results/" + file)
+
+with open('dict.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file)
+    for key, value in throughput_dict.items():
+        str = ';'.join(value)
+        writer.writerow([key, str])
+print throughput_dict
